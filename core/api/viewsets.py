5227124ad3ebce3f8dd342d7f18b1b331979b323
DevDosSantosLucas <lucas1995ls@outlook.com>
2023-02-04 15:26:29 -0300

    personalized actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -21,7 +21,20 @@ class TouristPlaceViewSet(viewsets.ModelViewSet):
         return Response({'return': request.data['name']})
     
     def destroy(self, request, *args, **kwargs):
-        pass
+        return super().destroy(request, *args, **kwargs)
+        
 
     def retrieve(self, request, *args, **kwargs):
-        pass 
+        return super().retrieve(request, *args, **kwargs)
+         
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @action(method =['get'],detail=True)
+    def denounce(self, request, pk=None):
+        pass
+
+    @action(method=['get'],detail=False)
+    def teste(self,request):
+        pass  
