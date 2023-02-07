83ba9e34f8e8ab965dac08ec29bc15497c505763
DevDosSantosLucas <lucas1995ls@outlook.com>
2023-02-07 20:50:11 -0300

    Serializers enhancing nested relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import tourist_places 
+from core.models import tourist_places
+from attractions.api.serializers import AttractionSerializer
+from localization.api.serializers import LocalizationSerializer
+
 
 class TouristPlacesSerializer(ModelSerializer):
+    attractions = AttractionSerializer(many=True)
+    address = LocalizationSerializer()
+
+
     class Meta:
         model = tourist_places
         fields = ('id', 'name','description','approved','attractions',
