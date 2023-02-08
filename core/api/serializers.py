a40f08351d54a50f429762d0708a087aa2b53bd5
DevDosSantosLucas <lucas1995ls@outlook.com>
2023-02-08 20:32:12 -0300

    many-to-many relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
+from attractions.models import attraction as Attraction
 from core.models import tourist_places
 from attractions.api.serializers import AttractionSerializer
 from localization.api.serializers import LocalizationSerializer
 
 class TouristPlacesSerializer(ModelSerializer):
-    attractions = AttractionSerializer(many=True)
-    address = LocalizationSerializer()
+    attractions = AttractionSerializer(many=True )#, read_only=True)
+    address = LocalizationSerializer(read_only=True)
     full_description = SerializerMethodField()
 
 
@@ -15,7 +16,20 @@ class TouristPlacesSerializer(ModelSerializer):
         fields = ('id', 'name','description','approved','attractions',
                   'comments','assessments','address','picture',
                   'full_description','full_description2')
-      
+        
+        read_only_fields = ('comments','assessments')
+
+    def create_attractions(self, attractions, place):
+        for attraction in attractions:
+            at = Attraction.objects.create(**attraction)
+            place.attractions.add(at)
     
+    def create(self, validated_data):
+        attractions = validated_data['attractions']
+        del validated_data['attractions']
+        place = tourist_places.objects.create(**validated_data)
+        self.create_attractions(attractions, place)
+        return place
+         
     def get_full_description(self, object):
         return '%s - %s' % (object.name,object.description)
